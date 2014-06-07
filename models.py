e39e06508021e02a2df773011214199a586225db
Mike Goddard <mikeygiom@gmail.com>
2014-06-08 00:28:29 +0100

    Added game elements to models and created tile model

--- models.py
+++ models.py
@@ -10,6 +10,11 @@ class User(Base):
     username = Column(String(50), unique=True)
     password = Column(String(128), unique=False)
     active_until = Column(DateTime, unique=False, default=(datetime.datetime.now))
+    x = Column(Integer, unique=False)
+    y = Column(Integer, unique=False)
+    direction = Column(Integer, unique=False)
+    character = Column(Integer, unique=False)
+    health = Column(Integer, unique=False)
 
     def __init__(self, username, password):
         self.username = username
@@ -18,6 +23,23 @@ class User(Base):
     def __repr__(self):
         return '<User %r>' % (self.username)
 
+class Tile(Base):
+    __tablename__ = 'tiles'
+    id = Column(Integer, primary_key=True)
+    x = Column(Integer)
+    y = Column(Integer)
+    tile_type = Column(Integer)
+    status = Column(Integer)
+
+    def __init__(self, x, y, tile_type, status):
+        self.x = x
+        self.y = y
+        self.tile_type = tile_type
+        self.status = status
+
+    def __repr__(self):
+        return '<Tile %r>' % (self.id)
+
 class Message(Base):
     __tablename__ = 'messages'
     id = Column(Integer, primary_key=True)
@@ -25,6 +47,8 @@ class Message(Base):
     created_at = Column(DateTime, unique=False, default=datetime.datetime.now)
     user_id = Column(Integer, ForeignKey('users.id'))
     user = relationship('User', backref=backref('messages', lazy='dynamic'))
+    x = Column(Integer)
+    y = Column(Integer)
 
     def __init__(self, text, user):
         self.text = text
